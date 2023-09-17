1cea339e9e46ce8e52951b1e407ed8d795195993
todd_wu <todd404@outlook.com>
2023-09-17 19:00:37 +0800

    🚮清理无效的测试

--- src/test/java/com/todd/judger/JudgerApplicationTests.java
+++ src/test/java/com/todd/judger/JudgerApplicationTests.java
@@ -11,22 +11,4 @@ import java.util.UUID;
 @SpringBootTest
 class JudgerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
-    @Test
-    void downloadFileTest() throws IOException {
-        File file = new File(  "temp/answer.txt");
-        file.createNewFile();
-        MyUtils.downloadFile("http://localhost:5000/1.txt", file.getPath());
-    }
-
-    @Test
-    void trimTest(){
-        String line = "\n";
-        String result = line.trim();
-        return;
-    }
-
 }
